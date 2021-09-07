d9e3c0957cce16d17a71a1d8f657cf31e9f7cb0a
zdxdsw <36400315+zdxdsw@users.noreply.github.com>
2021-09-07 18:00:47 -0400

    download shell

---
+++ download_imgs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+end=10
+x=1
+
+
+until [ $x -eq $end ]; do
+
+    wget http://tiger.lti.cs.cmu.edu/yingshac/WebQA_data_first_release/WebQA_imgs_7z_chunks/imgs.7z.00$x
+    x=$(($x+1))
+        
+done
+ 
+end=52
+until [ $x -eq $end ]; do
+    wget http://tiger.lti.cs.cmu.edu/yingshac/WebQA_data_first_release/WebQA_imgs_7z_chunks/imgs.7z.0$x
+    x=$(($x+1))
+done
